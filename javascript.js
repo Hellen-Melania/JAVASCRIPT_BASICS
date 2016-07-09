6d5d3c19826b646a9726757854df4911c25a7489
Hellen-Melania <alex_bonk@ukr.net>
2016-07-09 21:28:07 +0300

    add changes for hw

--- javascript.js
+++ javascript.js
@@ -14,20 +14,21 @@ var x1 = 0,
 	y1 = 0,
 	result,
 	X2 = prompt('Please enter X2 '),
-	Y2 = prompt('Please enter Y2 ');
+	Y2 = prompt('Please enter Y2 '),
+	xdiff = X2 - x1,
+	ydiff = Y2 - y1;
 
-	result = x1 + Number(X2) + y1 + Number(Y2);
-	console.log('The distance between points A(' + x1 + ',' + y1 +') and B(' + X2 + ',' + Y2 +') is ' + result + '.');	
+	result = Math.pow((xdiff * xdiff + ydiff * ydiff), 0.5);
+	console.log('The distance between points A(' + x1 + ',' + y1 +') and B(' + X2 + ',' + Y2 +') is ' + result +'.');	
 
 //task 3
 
 var sharp = '#', space = '';
 
-	for( var i = 0; i < 9; i++){
-		space += sharp;
-		console.log(space);  
-	}
-
+for( var i = 0; i < 9; i++){
+	space += sharp;
+	console.log(space);  
+}
 
 //task 4
 
@@ -50,40 +51,38 @@ var i = 0;
 		console.log(line);
 	}
 
-
 //task 5 Guess number
 
 var randomNunber = parseInt(Math.random() * 100, 10 );
 console.log('Random number is ' + randomNunber);
 var userGessNum = prompt('Please enter your number');
 
-// check if not empty
 
-while(!userGessNum){
+while(randomNunber){
+// check if not empty 
 	if (userGessNum == ''){
-		alert('You have not enered the number. Please, enter your number.');
+ 		alert('You have not enered the number. Please, enter your number.');
+ 		userGessNum = prompt('Please, enter your number again');
+ 	}
+	if (userGessNum < randomNunber ){
+		alert('Our number is higher, try again');
 		userGessNum = prompt('Please, enter your number again');
-		if (userGessNum === randomNunber){
+
+		if(userGessNum === randomNunber){
 			alert('You are right, the number is ' + randomNunber + '!');
+			break;
+		}
+	} 
+	else if(userGessNum > randomNunber ){
+		alert('Our number is lower, try again');
+		userGessNum = prompt('Please, enter your number again');
+		if(userGessNum === randomNunber){
+			alert('You are right, the number is ' + randomNunber + '!');
+			break;
 		}
 	}
-};
-
-if (userGessNum > randomNunber ){
-	alert('Our number is higher, try again');
-	userGessNum = prompt('Please, enter your number again');
-
-	if(userGessNum === randomNunber){
+	else{
 		alert('You are right, the number is ' + randomNunber + '!');
+		break;
 	}
-} 
-else if(userGessNum < randomNunber ){
-	alert('Our number is lower, try again');
-	userGessNum = prompt('Please, enter your number again');
-	if(userGessNum === randomNunber){
-		alert('You are right, the number is ' + randomNunber + '!');
-	}
-}
-else{
-	alert('You are right, the number is ' + randomNunber + '!');
 }
